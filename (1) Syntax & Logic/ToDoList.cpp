8ce33ce20bcd069be5063032fa1844f340301861
Raeco0520 <railand.lapiz180500@my.lsgh.edu.ph>
2026-02-14 12:12:38 +0800

    ToDoList WIP

--- (1) Syntax & Logic/ToDoList.cpp
+++ (1) Syntax & Logic/ToDoList.cpp
@@ -1,12 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
 // work in progress
 
 int main() {
-    string ListName ;
+    string ListName;
+    vector<string> List;
+
+    cout << "TO DO LIST PROGRAM\n";
+    cout << "Write List Name to Start : ";
+    cin >> ListName;
+    cout << endl;
+
+
 
 
 
     return 0;
 }
+
+
+/*
+TO DO LIST PROGRAM
+Plan to use
+getline(cin, input)
+clear() : Deletes all the bulletpoints
+pop back() : Delete latest bullet point
+push back() : Add new bulletpoint
+empty(): Checks if the list is empty
+begin() & end() : For iterating
+
+
+PSEUDO CODE PLAN
+
+instead of only cin, use getline(cin, input) to capture the whole line
+
+User must write the list name to start the program
+
+if user inputs "/delete", it deletes the most recent bullet point
+if user inputs "/clear", it clears all the bulletpoints
+    if there is no bulletpoints to delete, the program states that there is nothing to delete
+
+Every time the user enters a string, it adds as the latest bulletpoint
+and the program displays the title name reiterates from the oldest to the newest bulletpoint
+    If the input is an empty string, it will not be added as a new bulletpoint
+
+For more clarity, the action name is stated at the start after every successful input
+
+To get out of the program, user inputs "/break"
+
+*/
+
