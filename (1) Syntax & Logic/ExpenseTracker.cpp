e9150b5b07b3172b38f18a66a6056d8f00d691b0
Raeco0520 <railand.lapiz180500@my.lsgh.edu.ph>
2026-03-02 15:16:49 +0800

    Expense Tracker

---
+++ (1) Syntax & Logic/ExpenseTracker.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+
+//It's prolly just gonna be a more complex version of the To Do List
+
+void clear() {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+
+void displayTable () {
+}
+
+
+
+char select() {
+}
+
+
+int main () {
+    std::vector<std::string> itemDefinition{};
+    std::vector<double> itemCost{};
+    char mode{};
+
+ 
+
+    return 0;
+}
