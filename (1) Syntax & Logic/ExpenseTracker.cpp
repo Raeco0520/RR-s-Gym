bd93ccfa8af27f2df3e7cf2522888f02a5a79bc4
Raeco0520 <railand.lapiz180500@my.lsgh.edu.ph>
2026-03-03 14:03:36 +0800

    Expense Tracker WIP

--- (1) Syntax & Logic/ExpenseTracker.cpp
+++ (1) Syntax & Logic/ExpenseTracker.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <limits>
+#include <vector>
+
+
 
 //It's prolly just gonna be a more complex version of the To Do List
 
@@ -11,20 +14,50 @@ void clear() {
 
 
 void displayTable () {
+    std::cout << "=================";
+    std::cout << "Expense Tracker";
+    std::cout << "================";
+    std::cout << "Item | Cost ";
+
+    // Insert vectors here (item definition and item cost)
+    // plan to make '=' and '|' to be dynamic depending on the length of the total characters within the row
+    // kinda like python if they have it idk
+
+    std::cout << "================";
+
 }
 
 
-
-char select() {
-}
 
 
 int main () {
     std::vector<std::string> itemDefinition{};
     std::vector<double> itemCost{};
-    char mode{};
+    int mode{};
 
- 
+    std::cout << "Choose Mode\n";
+    std::cout << "0 - End Program\n";
+    std::cout << "1 - Add Item\n";
+    std::cout << "2 - Delete Item\n";
+    std::cout << "3 - Edit Item\n";
+    std::cout << "Selection: ";
+
+
+    while (!(std::cin >> mode) || mode < 0 || mode > 3) {
+        std::cout << "Invalid Input : ";
+        clear();
+        }
+
+    switch (mode) {
+        case 0 :
+
+        case 1 :
+
+        case 2 :
+
+        case 3 :
+
+        }
 
     return 0;
 }
